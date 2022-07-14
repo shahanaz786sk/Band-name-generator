a713e815598ad78bcb05f6af057c38ce22aded23
shahanaz786sk <109270156+shahanaz786sk@users.noreply.github.com>
2022-07-14 03:44:58 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Welcome to the bandname generator")
+#2. Ask the user for the city that they grew up.
+city = input("Which city did you grow up in?\n")
+#3. Asl user the name of their pet.
+pet = input("What is the name of your pet?\n")
+#. Cmbine city name and pet name and show the result as their band name.
+print("Your band name could be " + city + " " + pet)
